79013a655cf2899115305cd44cbf6feaa24cf28e
Diana <37572031+XxoSio@users.noreply.github.com>
2022-12-04 18:09:29 +0900

    mm.c mm_malloc(size_t size)
    mm.c mm_malloc(size_t size)

--- mm.c
+++ mm.c
@@ -258,6 +258,22 @@ void *coalesce(void *bp)
     return bp;
 }
 
+/* 
+ * mm_malloc - Allocate a block by incrementing the brk pointer.
+ *     Always allocate a block whose size is a multiple of the alignment.
+ */
+void *mm_malloc(size_t size)
+{
+    int newsize = ALIGN(size + SIZE_T_SIZE);
+    void *p = mem_sbrk(newsize);
+    if (p == (void *)-1)
+	return NULL;
+    else {
+        *(size_t *)p = size;
+        return (void *)((char *)p + SIZE_T_SIZE);
+    }
+}
+
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
